22227c992e13f41eaae2a2709fd2831c37e7c055
gongganghao <2908273068@qq.com>
2023-11-06 17:03:46 +0800

    Fixed issue # 添加缩放弹性系数

--- TransformIcon.ts
+++ TransformIcon.ts
@@ -7,8 +7,8 @@ const { regClass, property } = Laya;
 @regClass()
 export default class TransformIcon extends Laya.Script {
 
-   
-   
+
+
     //最小范围就是1不允许修改
     public minScale: number = 1;
     @property({ type: Number, tips: "最大缩放范围" })
@@ -16,29 +16,33 @@ export default class TransformIcon extends Laya.Script {
     @property({ type: Number, tips: "托动速度" })
     public dragSpeed: number = 2;
     @property({ type: Number, min: 0, max: 0.5, tips: "弹性缩放范围" })
-    public flexScale: number = 0.2;
+    public flexScale: number = 0.;
     @property({ type: Number, tips: "弹性位置范围" })
-    public flexPosition: number = 100;
+    public flexPosition: number = 300;
 
 
 
     private _scaleStep: number = 0.005;
+    private _scalePercent: number = 1;
     private _positionStep: number = 5;
+    private _positionPercent: number = 0;
     private _minX: number = 0;
     private _maxX: number = 0;
     private _minY: number = 0;
     private _maxY: number = 0;
 
 
-    
+
     private _owner: Laya.Image;
-     /** 是否锁定位置 */
-    private _isLockPos:boolean = false;
+    /** 是否锁定位置 */
+    private _isLockPos: boolean = false;
     /** 临时数值 */
     private _tempValue: number = 0;
+    /** 临时方向 */
+    private _tempDir: Laya.Vector2 = new Laya.Vector2();
     /** 缩放前的中心点坐标信息 */
     private _lastPivot: Laya.Point = new Laya.Point();
-   
+
 
     onEnable(): void {
         this._owner = this.owner as Laya.Image;
@@ -66,10 +70,12 @@ export default class TransformIcon extends Laya.Script {
         //设置初始值
         this._minX = parentBox.width - this._owner.displayWidth;
         this._minY = parentBox.height - this._owner.displayHeight;
+
     }
 
 
     onMouseDown(e: Laya.Event): void {
+        this._isHandUp = false;
         Laya.stage.on(Laya.Event.MOUSE_MOVE, this, this.onMouseMove);
         if (e.touches && e.touches.length > 1) {
             this._lastPivot = this.getPivot(e);
@@ -83,36 +89,41 @@ export default class TransformIcon extends Laya.Script {
 
     /** 鼠标（或手势）在对象上移动时触发的事件侦听方法 */
     onMouseMove(e: Laya.Event): void {
-        if (e.touches && e.touches.length > 1) {
-            /**当前的双指距离*/
-            let distance = this.getDistance(e);
-            //设置缩放
-            let scale = this._owner.scaleX + (distance - this._tempValue) * this._scaleStep;
-            this.setScale(scale);
-            this._tempValue = distance;
-            let curPiovt =  this.getPivot(e);
-            this.setPositin(new Laya.Vector2(curPiovt.x-this._lastPivot.x,curPiovt.y-this._lastPivot.y))
-        }else{
-            this.setPositin(new Laya.Vector2(this._owner.mouseX-this._lastPivot.x,this._owner.mouseY-this._lastPivot.y))
+        if(!this._isHandUp){
+            if (e.touches && e.touches.length > 1) {
+                /**当前的双指距离*/
+                let distance = this.getDistance(e);
+                //设置缩放
+                let scale = this._owner.scaleX + (distance - this._tempValue) * this._scaleStep * this._scalePercent;
+                if (this.setScale(scale)) {
+                    this._tempValue = distance;
+                    let curPiovt = this.getPivot(e);
+                    this.setPositin(new Laya.Vector2(curPiovt.x - this._lastPivot.x, curPiovt.y - this._lastPivot.y))
+                }
+            } else {
+                this.setPositin(new Laya.Vector2(this._owner.mouseX - this._lastPivot.x, this._owner.mouseY - this._lastPivot.y))
+            }
         }
-
+       
     }
 
+    private _isHandUp:boolean;
     onMouseUp(e: Laya.Event): void {
+        this._isHandUp = true;
         Laya.stage.off(Laya.Event.MOUSE_MOVE, this, this.onMouseMove);
         this.flexEffect()
     }
 
 
     //回弹效果
-    flexEffect(){
-        if (this._owner.scaleX < this.minScale || this._owner.scaleX  > this.maxScale) {
+    flexEffect() {
+        if (this._owner.scaleX < this.minScale || this._owner.scaleX > this.maxScale) {
             this._isLockPos = true;
-            let targetScale = (this._owner.scaleX <= this.minScale) ? this.minScale : this.maxScale; 
+            let targetScale = (this._owner.scaleX <= this.minScale) ? this.minScale : this.maxScale;
             let offsetX = this._owner.x - this._lastPivot.x * (targetScale - this._owner.scaleX);
             let offsetY = this._owner.y - this._lastPivot.y * (targetScale - this._owner.scaleX);
-            let targetPos = (this._owner.scaleX <= this.minScale) ? new Laya.Vector2(0,0) : new Laya.Vector2(offsetX,offsetY); 
-            Laya.Tween.to(this._owner, {scaleX: targetScale,scaleY: targetScale,x:targetPos.x,y:targetPos.y}, 200,Laya.Ease.circOut,Laya.Handler.create(this,()=>{
+            let targetPos = (this._owner.scaleX <= this.minScale) ? new Laya.Vector2(0, 0) : new Laya.Vector2(offsetX, offsetY);
+            Laya.Tween.to(this._owner, { scaleX: targetScale, scaleY: targetScale, x: targetPos.x, y: targetPos.y }, 200, Laya.Ease.circOut, Laya.Handler.create(this, () => {
                 this._isLockPos = false;
                 this.updateLimit();
             }))
@@ -120,14 +131,14 @@ export default class TransformIcon extends Laya.Script {
         if (this._owner.x < this._minX || this._owner.x > this._maxX || this._owner.y < this._minY || this._owner.y > this._maxY) {
             this._isLockPos = true;
             let targetX = this._owner.x;
-            if(this._owner.x < this._minX || this._owner.x > this._maxX){
+            if (this._owner.x < this._minX || this._owner.x > this._maxX) {
                 targetX = (this._owner.x <= this._minX) ? this._minX : this._maxX;
             }
             let targetY = this._owner.y;
-            if(this._owner.y < this._minY || this._owner.y > this._maxY){
+            if (this._owner.y < this._minY || this._owner.y > this._maxY) {
                 targetY = (this._owner.y <= this._minY) ? this._minY : this._maxY;
             }
-            Laya.Tween.to(this._owner, { x: targetX, y: targetY}, 200,Laya.Ease.circOut,Laya.Handler.create(this,()=>{
+            Laya.Tween.to(this._owner, { x: targetX, y: targetY }, 200, Laya.Ease.circOut, Laya.Handler.create(this, () => {
                 this._isLockPos = false;
             }))
         };
@@ -146,58 +157,98 @@ export default class TransformIcon extends Laya.Script {
         return distance * 2;
     }
 
-    
-    private setPositin(direction:Laya.Vector2) {
-        if(Laya.Vector2.scalarLength(direction) >= this._positionStep){
-            let offsetX = this._owner.x + direction.x*this.dragSpeed;
-            let offsetY = this._owner.y + direction.y*this.dragSpeed;
-            this.detectPosition(offsetX,offsetY);
+
+    private setPositin(direction: Laya.Vector2) {
+        if (Laya.Vector2.scalarLength(direction) >= this._positionStep) {
+            if(this._positionPercent < 1){
+                Laya.Vector2.normalize(direction,this._tempDir);
+            }else{
+                this._tempDir = direction.clone();
+            }
+            let offsetX = this._owner.x + this._tempDir.x * this.dragSpeed * this._positionPercent;
+            let offsetY = this._owner.y + this._tempDir.y * this.dragSpeed * this._positionPercent;
+            this.detectPosition(offsetX, offsetY);
         }
-       
+
     }
 
-    private detectPosition(offsetX:number,offsetY:number){
-        if(this._isLockPos){
+    private detectPosition(offsetX: number, offsetY: number) {
+        if (this._isLockPos) {
             return;
         }
 
-        
-        if (offsetX < this._minX-this.flexPosition) {
-            this._owner.x = this._minX-this.flexPosition;
+        let scale = this._owner.scaleX;
+        if (offsetX < this._minX || offsetX > this._maxX || offsetY < this._minY || offsetY > this._maxY) {
+            let targetX = this._owner.x < this._minX ? this._minX : this._maxX;
+            let targetY = this._owner.y < this._minY ? this._minY : this._maxY;
+            let dis = Math.min(Math.abs(targetX - offsetX) , Math.abs(targetY - offsetY));
+            this._positionPercent = Math.max(0, (1 - dis / (this.flexPosition*scale)*2));
+        } else {
+            this._positionPercent = 1;
         }
-        else if (offsetX > this._maxX+this.flexPosition) {
-            this._owner.x = this._maxX+this.flexPosition;
+
+     
+
+        if (offsetX < this._minX - this.flexPosition*scale) {
+            this._owner.x = this._minX - this.flexPosition*scale;
+        }
+        else if (offsetX > this._maxX + this.flexPosition*scale) {
+            this._owner.x = this._maxX + this.flexPosition*scale;
         }
         else {
             this._owner.x = offsetX;
         }
 
-        if (offsetY < this._minY-this.flexPosition) {
-            this._owner.y = this._minY-this.flexPosition;
+        if (offsetY < this._minY - this.flexPosition*scale) {
+            this._owner.y = this._minY - this.flexPosition*scale;
         }
-        else if (offsetY > this._maxY+this.flexPosition) {
-            this._owner.y = this._maxY+this.flexPosition;
+        else if (offsetY > this._maxY + this.flexPosition*scale) {
+            this._owner.y = this._maxY + this.flexPosition*scale;
         }
         else {
             this._owner.y = offsetY;
-        }  
+        }
+
+       
+    }
+
+    private lockPosition() {
+        let parentBox = this._owner.parent as Laya.Box;
+        //设置初始值
+        let posX = (parentBox.width - this._owner.displayWidth) / 2;
+        let posY = (parentBox.height - this._owner.displayHeight) / 2;
+        this._owner.pos(posX, posY)
     }
 
 
     private setScale(value: number) {
-        if (value > (this.maxScale*(this.flexScale+1)) || value < (this.minScale *(1-this.flexScale))) {
-            return;
-        };
+        if (value > (this.maxScale * (this.flexScale + 1)) || value < (this.minScale * (1 - this.flexScale))) {
+            return false;
+        }
+        if (value >= this.maxScale) {
+            this._isLockPos = false;
+            this._scalePercent = Math.max(0, (1 - value / (this.maxScale * (this.flexScale + 1)))*2);
+        }
+        else if (value <= this.minScale) {
+            this._isLockPos = true;
+            this._scalePercent = Math.min(1, (value / (this.minScale * (1 - this.flexScale)) - 1) / 100);
+        } else {
+            this._isLockPos = false;
+            this._scalePercent = 1;
+        }
+
         let lastScale = this._owner.scaleX;
         this._owner.scale(value, value);
-        if(value >= this.minScale){
+        if (value >= this.minScale) {
             this.updateLimit();
-        }else{
-            return;
+        } else {
+            this.lockPosition()
+            return false;
         }
         let offsetX = this._owner.x - this._lastPivot.x * (value - lastScale);
         let offsetY = this._owner.y - this._lastPivot.y * (value - lastScale);
-        this.detectPosition(offsetX,offsetY);
+        this.detectPosition(offsetX, offsetY);
+        return true;
     }
 
     /**
@@ -205,9 +256,9 @@ export default class TransformIcon extends Laya.Script {
      * @param touches 手势信息数组
      */
     getPivot(e: Laya.Event): Laya.Point {
-        let Point0: Laya.Point = this._owner.globalToLocal(new Laya.Point(e.touches[0].pos.x, e.touches[0].pos.y));
-        let Point1: Laya.Point = this._owner.globalToLocal(new Laya.Point(e.touches[1].pos.x, e.touches[1].pos.y));
-        return new Laya.Point((Point0.x + Point1.x) / 2, (Point0.y + Point1.y) / 2);
+        let Point0: Laya.Point = new Laya.Point(e.touches[0].pos.x, e.touches[0].pos.y);
+        let Point1: Laya.Point = new Laya.Point(e.touches[1].pos.x, e.touches[1].pos.y);
+        return this._owner.globalToLocal(new Laya.Point((Point0.x + Point1.x) / 2, (Point0.y + Point1.y) / 2));
     }
 
     onDisable(): void {
